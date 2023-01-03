2e1c5d46edc2f0a00b1b181852779d846add3953
Sebthecreator95 <73322507+Sebthecreator95@users.noreply.github.com>
2023-01-02 20:26:45 -0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,5 @@
 function camelcase(s) {
-    
+    //"find how many words the 'camelCased' string has"
     let count = 1
     for(let i = 0; i < s.length - 1; i++){
         if(s[i] === s[i].toUpperCase()){
